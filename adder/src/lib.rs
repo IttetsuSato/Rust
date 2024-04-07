63cb9fdc84a877f3bcec7c96b3522afc9162a2bb
Ittetsu Sato <ittetsusato@gmail.com>
2024-04-08 08:27:52 +0900

    feat: learn test

---
+++ adder/src/lib.rs
@@ -0,0 +1,64 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+// assertマクロが引数をデバッグフォーマットでプリントするので、比較対象の値はDebugトレイトを実装している必要がある
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works2() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
---
+++ adder/target/debug/deps/adder-953e7356920a5859.d
@@ -0,0 +1,5 @@
+/Users/ittetsu.sato/projects/adder/target/debug/deps/adder-953e7356920a5859: src/lib.rs
+
+/Users/ittetsu.sato/projects/adder/target/debug/deps/adder-953e7356920a5859.d: src/lib.rs
+
+src/lib.rs:
